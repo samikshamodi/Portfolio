40a056269a411fd1c01084787011c18394d11a47
samikshamodi <samikshamodi7@gmail.com>
2021-03-09 17:27:07 +0530

    Done

--- resources/app.js
+++ resources/app.js
@@ -22,38 +22,19 @@ function type(){
 /* 
 const t1=gsap.timeline({defaults:{ease: "power1.out"}})
 
-t1.to('.hello',{y:"0vh",duration:1})
 t1.to('.name2',{x:"0%",duration:1},"-=1")
  t1.to('.typing',{x:"0%",duration:1},"-=1") 
  t1.fromTo('.hello',{opacity:0},{opacity:1,duration:1})  
  t1.fromTo('header',{opacity:0},{opacity:1,duration:1}); */
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
- setTimeout(type,3000);
-
-
- // Wrap every letter in a span
- var textWrapper = document.querySelector('.readJournal .letters2');
- textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
- 
- anime.timeline({loop: true})
-   .add({
-     targets: '.readJournal .letter',
-     scale: [0, 1],
-     duration: 1500,
-     elasticity: 600,
-     delay: (el, i) => 45 * (i+1)
-   }).add({
-     targets: '.readJournal',
-     opacity: 0,
-     duration: 1000,
-     easing: "easeOutExpo",
-     delay: 1000
-   });
+tl.to(".text", { y: "0%", duration: 0.2});
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.1 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 
 
 
-
-   anime.timeline({loop: true})
+   anime.timeline({loop:1})
    .add({
      targets: '.ml8 .circle-white',
      scale: [0, 3],
@@ -84,12 +65,6 @@ t1.to('.name2',{x:"0%",duration:1},"-=1")
      rotateZ: [45, 15],
      duration: 1200,
      offset: '-=1000'
-   }).add({
-     targets: '.ml8',
-     opacity: 0,
-     duration: 1000,
-     easing: "easeOutExpo",
-     delay: 1400
    });
  
  anime({
@@ -99,3 +74,29 @@ t1.to('.name2',{x:"0%",duration:1},"-=1")
    easing: "linear",
    loop: true
  });
+
+  // Wrap every letter in a span
+  var textWrapper = document.querySelector('.readJournal .letters2');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  
+  anime.timeline({loop: true})
+    .add({
+      targets: '.readJournal .letter',
+      scale: [0, 1],
+      duration: 1500,
+      elasticity: 600,
+      delay: (el, i) => 45 * (i+1)
+    }).add({
+      targets: '.readJournal',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
+
+
+    tl.fromTo(".name2", { opacity: 0 }, { opacity: 1, duration: 1.5,delay: 4  });
+    tl.fromTo(".typing", { opacity: 0 }, { opacity: 1, duration: 1.5,  });
+    tl.fromTo("header", { opacity: 0 }, { opacity: 1, duration: 1  },);
+ 
+    setTimeout(type,7000);
